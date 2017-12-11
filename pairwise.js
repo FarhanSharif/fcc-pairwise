2dcce0c20ada18031da47913cce06558ac4bbf47
Farhan Sharif <farhanisharif@gmail.com>
2017-12-11 14:51:46 -0500

    second attempt (works)
    fixed: if index used, ignores them on next iterations

--- pairwise.js
+++ pairwise.js
@@ -4,13 +4,26 @@ function pairwise(arr, arg) {
   var curr; // current static
   var shuffle; // number after curr shuffled through until either sum found or end reached
   var sum = 0;
+  var alreadyUsed = makeZeroArray(arr.length); //array of indexes (1 - used, 0 - not)
   
   for (var i = 0; i < arr.length; i++) {
+    
+    // if 1, then index already used and cannot be used again
+    if (alreadyUsed[i] != 0)
+      continue;
+
     curr = arr[i];
     
     for (var j = i+1; j < arr.length; j++) {
+
+      // if 1, then index already used and cannot be used again
+      if (alreadyUsed[j] != 0)
+        continue;
+
       shuffle = arr[j];
       if (curr + shuffle == arg) {
+        alreadyUsed[i] = 1; // index now has been used
+        alreadyUsed[j] = 1; // index now has been used
         sum += i + j;
         break;
       }
@@ -20,6 +33,15 @@ function pairwise(arr, arg) {
   
   console.log(sum);
   return sum;
+}
+
+function makeZeroArray(length) {
+  
+  var arr = [];
+  for (var i = 0; i < length; i++)
+    arr[i] = 0;
+
+  return arr;
 }
 
 pairwise([1,4,2,3,0,5], 7);
